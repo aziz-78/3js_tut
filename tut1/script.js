1fbdd0b9f3af4be66907a6f7ee7e56cbe2853864
aziz_78 <aziz.fre9@gmail.com>
2023-10-11 15:58:54 +0500

    finished first tut

--- tut1/script.js
+++ tut1/script.js
@@ -1,21 +1,32 @@
-const scene = new THREE.Scene()
-const element = document.querySelector(".webgl")
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({color:0xff0000})
-const mesh = new THREE.Mesh(geometry,material)
-scene.add(mesh) 
+// script.js
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);// sets the vertical view,aspect_ratio,near-clipping,far clipping
 
-const sizes  = {
-    width:800,
-    height:600
-}
+const canvas = document.querySelector("#webgl")// gets the canvas element from index.html
+const renderer = new THREE.WebGLRenderer({canvas: canvas}); //creating the render and specifying it to render in canvas we got from html
+renderer.setSize(window.innerWidth, window.innerHeight);//sets the render size
 
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 2
-scene.add(camera)
-console.log(element)
-const renderer = new THREE.WebGLRenderer({
-    canvas:element
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+const geometry = new THREE.BoxGeometry(2,2,2); //specifies the material shape and size
+const texture = new THREE.TextureLoader().load('images.png');
+//const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 ,wireframe:true });     //wireframe
+const material = new THREE.MeshBasicMaterial({ color: 0x0000ff ,map:texture }); //image on all sides of box
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 10;
+
+const animate = () => {
+    requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    
+
+    renderer.render(scene, camera);
+    
+    
+    
+};
+
+animate();
